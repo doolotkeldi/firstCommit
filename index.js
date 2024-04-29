131e23f322f0008d76adae951994b3efa3c80b33
doolotkeldi <dolotalanov35@gmail.com>
2024-04-29 17:36:25 +0600

    forst commit

---
+++ index.js
@@ -0,0 +1,15 @@
+// let odject = {
+//     name:"doolotkeldi",
+//     age:18,
+
+// }
+
+// localStorage.setItem("person",JSON.stringify(odject))
+// const row = localStorage.getItem("person")
+// const person = JSON.parse(row)
+// person.name="kazymalyly"
+// console.log(person)
+
+let data = {
+    image 
+}
